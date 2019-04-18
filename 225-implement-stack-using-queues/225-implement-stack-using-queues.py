636fea1f7dd7d43347cf9ed4de433392be32b7d1
Cong Luwen <congve1@live.com>
2019-04-18 17:05:20 +0800

    225

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -1,9 +1,12 @@
+from collections import deque
 class MyStack(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.enter = deque()
+        self.exit = deque()
         
 
     def push(self, x):
@@ -12,6 +15,9 @@ class MyStack(object):
         :type x: int
         :rtype: None
         """
+        self.exit.append(x)
+        while(len(self.exit) > 1):
+            self.enter.append(self.exit.popleft())
         
 
     def pop(self):
@@ -19,6 +25,12 @@ class MyStack(object):
         Removes the element on top of the stack and returns that element.
         :rtype: int
         """
+        top = self.exit.popleft()
+        while(len(self.enter) > 1):
+            self.exit.append(self.enter.popleft())
+        self.exit, self.enter = self.enter, self.exit
+        return top
+        
         
 
     def top(self):
@@ -26,6 +38,7 @@ class MyStack(object):
         Get the top element.
         :rtype: int
         """
+        return self.exit[0]
         
 
     def empty(self):
@@ -33,6 +46,7 @@ class MyStack(object):
         Returns whether the stack is empty.
         :rtype: bool
         """
+        return len(self.exit) == 0 and len(self.enter) == 0
         
 
 
--- 225-implement-stack-using-queues/README.md
+++ 225-implement-stack-using-queues/README.md
@@ -33,13 +33,20 @@ Implement the following operations of a stack using queues.
 **Example:**
 
     
-    
-    MyStack stack = new MyStack();
-    
-    stack.push(1);
-    stack.push(2);  
-    stack.top();   // returns 2
-    stack.pop();   // returns 2
+    
+
+    MyStack stack = new MyStack();
+
+    
+
+    stack.push(1);
+
+    stack.push(2);  
+
+    stack.top();   // returns 2
+
+    stack.pop();   // returns 2
+
     stack.empty(); // returns false
 
 **Notes:**
