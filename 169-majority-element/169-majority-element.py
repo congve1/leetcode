be8c7252fc968bf05d42cb22c7189ef31b88465c
Cong Luwen <congve1@live.com>
2019-04-19 08:59:00 +0800

    169 206

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        count = 1
+        num = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] == num:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    num = nums[i+1]
+        return num
--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -3,11 +3,32 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+# 迭代版本
 class Solution(object):
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        
+        cur = head
+        prev = None
+        while cur:
+            next_temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next_temp
+        return prev
+
+# 递归版本
+class Solution_2(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
